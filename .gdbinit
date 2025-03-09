2be79e1832ed8d8609078ebc158b35d1ad4f08fe
sodacris <wjxa20152015@gmail.com>
2025-03-09 18:28:04 +0800

    add LLDB visualizer

---
+++ .gdbinit
@@ -0,0 +1,5 @@
+python
+import sys
+sys.path.insert(0, '.')
+import viz
+end
---
+++ viz.cpp
@@ -0,0 +1,29 @@
+namespace MyNamespace {
+class MyClass {
+public:
+  int member1;
+  int member2;
+};
+} // namespace MyNamespace
+
+class Rectangle {
+private:
+  int height;
+  int width;
+
+public:
+  Rectangle() : height(3), width(5) {}
+  Rectangle(int H) : height(H), width(H * 2 - 1) {}
+  Rectangle(int H, int W) : height(H), width(W) {}
+  int GetHeight() { return height; }
+  int GetWidth() { return width; }
+};
+
+int main() {
+  MyNamespace::MyClass a;
+  a.member1 = 4;
+  a.member2 = 2;
+
+  Rectangle r;
+  return 0;
+}
